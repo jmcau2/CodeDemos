4f377a1a4bf7f8dc8d24026aaf33aa63b8c9f403
darkshadowofoxdeath <jmcau2@uis.edu>
2016-08-23 18:55:29 +0200

    Update CustomerLookupForm.cs
    Commented.

--- CustomerLookupForm.cs
+++ CustomerLookupForm.cs
@@ -16,6 +16,8 @@ namespace PeoriaTechnologyIntakeSheet
 {
     public partial class CustomerLookupForm : Form
     {
+        
+        //Initializing Form
         Form1 frm1;
         public CustomerLookupForm(Form1 parent)
         {
@@ -25,14 +27,23 @@ namespace PeoriaTechnologyIntakeSheet
         }
 
        
-
+        //Populating the list of all the customers
         private void populateList()
         {
+            //This particular line Deserializes a JSON response from the server.
+            //It makes a Get Request and sends the DB connection info to a php script via a POST action, it POSTS
+            //the connection string which is set in a XML configuration file.  From there the PHP script will ping the MYSQL DB
+            //and then in turn the information is sent back to this application in JSON format and is deserialized into a list
+            //of type Customers
             Customers currentCustomers = JsonConvert.DeserializeObject<Customers>(DataBase.Get("get_customer_info.php", Config.dbConnectString()));
 
+            //We shrink the size by one to account for the 0 index beggining
             int currentCustomersSize = currentCustomers.getSize() - 1;
 
+            //After the customers list has been retreived we open up the form, got to make sure we aren't passing a bunch of null values.
             InitializeComponent();
+            
+            //Adding the Customers to the datagrid view.
             for (int i = currentCustomersSize; i >= 0; i--)
             {
 
@@ -41,23 +52,28 @@ namespace PeoriaTechnologyIntakeSheet
             }   
         }
 
+        //The datagrid view is set to select the entire row, from here we grab the customer info and load up the last invoice they had
         private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
 
             selectCustomer();
         }
 
+        //This has the same effect as the above double click, of course it's named button1
         private void button1_Click(object sender, EventArgs e)
         {
            selectCustomer();
         }
 
+        //This clears out the list then re-runs the populate list method to reload any changes that may have occured since the
+        //last load
         private void refreshButton_Click(object sender, EventArgs e)
         {
             dataGridView1.Rows.Clear();
             populateList();
         }
 
+        //Grabs all of the information about the customer and slaps it into an editable form/invoice so you can close out, update, etc.
         private void selectCustomer()
         {
             foreach (DataGridViewRow row in dataGridView1.SelectedRows)
